b65d0cc8cbe85570d2969c40d99e46cc2351ce00
salmanyuris <salmanyuris45@gmail.com>
2026-09-11 22:11:22 +0700

    commit web dasar assesment

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollToTopButton").style.display = "block";
+    } else {
+        document.getElementById("scrollToTopButton").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
